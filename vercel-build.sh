a0d399b07c2f149778b64c54d6f0ab751389bf51
Manas Arora <aroramanas01@gmail.com>
2025-06-01 13:55:02 +0530

    wse

--- vercel-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PHP_INI_SCAN_DIR=/vercel/path0
-export PHP_EXTENSIONS="mongodb,curl,openssl,json,mbstring,fileinfo"
-composer install --no-dev --optimize-autoloader
-composer dump-autoload --optimize 
